1e325bf87b824a5e5526407d2e54b5c0a1faf082
Pooja <poojarajput9516@gmail.com>
2023-04-24 00:07:22 +0530

    user sign in sign up

--- src/components/user/signIn/signin.js
+++ src/components/user/signIn/signin.js
@@ -1,67 +1,45 @@
-import './signin.css'
+import "./signin.css";
+
 function SignIn(){
-
     return <>
-     <div className="breadcrumbs-area ">
-        <div className="container">
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="breadcrumbs-menu">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#" className="active">SignIn</a></li>
-                        </ul>
-                    </div>
+        {/* Hello world */}
+        <div className="container-fluid">
+            <div className="logimain">
+                <div className="loginform_div">
+                    <form action="/signin" method="post">
+                        <div className="formHeading">
+                            <p>Registered Customers</p>
+                        </div>
+                        <div className="peraLogin">
+                            <p>If you have an account with us, please log in.</p>
+                        </div>
+                        <div>
+                            <div className="form-group">
+                                <label className="loginformlevel">Email Address </label>
+                                <input type="email" name="email" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label className="loginformlevel">Password</label>
+                                <input type="password" name="password" className="form-control" />
+                            </div>
+                            <div className="loginformbtn">
+                                <button type="submit">Login</button>
+                                <span className="loginforgotpassword">
+                                    
+                                    Forgot Your Password?
+                                </span>
+                                <a href="/signup" style={{ float: "right" }} className="newUser">
+                                    New user ?
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-    <div className="container-fluid ps-md-0">
-  <div className="row g-0">
-    <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-    <div className="col-md-8 col-lg-6">
-      <div className="login d-flex align-items-center py-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-9 col-lg-8 mx-auto">
-              <h3 className="login-heading mb-4">Welcome back!</h3>
-
-             
-              <form>
-                <div className="form-floating mb-3">
-                  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                  <label for="floatingInput">Email address</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                  <label for="floatingPassword">Password</label>
-                </div>
-
-                <div className="form-check mb-3">
-                  <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
-                  <label className="form-check-label" for="rememberPasswordCheck">
-                    Remember password
-                  </label>
-                </div>
-
-                <div className="d-grid">
-                  <button className="btn btn-lg  btn-login text-uppercase fw-bold mb-2  btn btn-dark" type="submit">Sign in</button>
-                  <div className="text-center">
-                    <a className="small" href="#">Forgot password?</a>
-                  </div>
-                </div>
-
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+    </>
 
     
-    </>
 }
 
-export default SignIn;
+export default SignIn ;
