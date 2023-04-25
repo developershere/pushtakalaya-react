9b92c473f947f43f73b261708d328236dfa3f98f
Chetna <rathorechetna03@gmail.com>
2023-04-25 10:57:37 +0530

    product add

--- src/router-config/categorySlice.js
+++ src/router-config/categorySlice.js
@@ -5,7 +5,7 @@ import { apiEndPoint } from "../webApi/webapi";
 export const fetchCategory = createAsyncThunk('fetchCategory',async ()=>{
   let response = await axios.get(apiEndPoint.CATEGORY_API);
   if(response.data.status){
-    console.log(response.data.category)
+    console.log(response.data)
   return response.data.category
   }
 })
