eaa647ec6e20fd280dc3765de58389a869c765ab
sanjupatel2308 <sanjupatel2308@gmail.com>
2023-04-28 15:21:24 +0530

    template Edited

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -4,7 +4,7 @@ function Banner(){
         <div className="container ">
             <div className="row ">
                 <div className="col-lg-6 col-md-6 col-12 ">
-                    <div className="banner-shadow-hover xs-mb ">
+                    <div className="banner-shadow-hover xs-mb " >
                         <a href="# "><img src="img/banner/8.jpg " alt="banner " /></a>
                     </div>
                 </div>
--- src/components/user/signup/signup.js
+++ src/components/user/signup/signup.js
@@ -80,7 +80,7 @@ function SignUp() {
                 </div>
             </div>
         </div>
-        <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+        <section className="vh-100">
             <div className="container h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col-lg-12 col-xl-11">
@@ -88,17 +88,16 @@ function SignUp() {
                         <div className="row justify-content-center">
                             <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-2 order-lg-1" >
                                 <img
-                                    src="https://storage.googleapis.com/prod-relayread-com/static/frontend/images/flow-resp.png"
+                                    src=""
                                     className="img-fluid" style={{ borderRadius: "0px 10% 0% 10%", boxShadow: "0px 0px 15px gray" }}
                                     alt="Sample image"
                                 />
                             </div>
                             <div className="col-md-10 col-lg-6 col-xl-5 order-1 order-lg-2">
-                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style={{color:"#f07c29", textShadow:"2px 2px 2px gray"}}>
                                     Sign up
                                 </p>
                                 <form onSubmit={handleSubmit}>
-                                    <i className="fas fa-user fa-lg me-3 fa-fw" />
                                     <div className="form-group">
 
                                         <input onChange={(event) => setName(event.target.value)} type="text" placeholder="Enter name" className="form-control" />
@@ -113,7 +112,7 @@ function SignUp() {
                                         <input onChange={(event) => setContact(event.target.value)} type="text" placeholder="Enter contact number" className="form-control" />
                                     </div>
                                     <div className="form-group text-center">
-                                        <button onClick={(()=>verifyEmail(email,name))} type="submit" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                                        <button onClick={(()=>verifyEmail(email,name))} type="submit" className="btn submitbtn w-100" data-toggle="modal" data-target="#exampleModalCenter" >
                                             Sign Up
                                         </button>
                                     </div>
@@ -139,7 +138,7 @@ function SignUp() {
                                                         <input onChange={(event) => setOtp(event.target.value)} class="m-2 text-center form-control rounded width:10" type="text" id="fourth" maxlength="4" />
                                                         </div>
                                                         <div class="mt-4">
-                                                         <button onClick={()=>registration(otp)} class="btn btn-danger px-4 validate">Validate</button> 
+                                                         <button onClick={()=>registration(otp)} class="btn btn-warning px-4 validate">Validate</button> 
                                                         </div>
                                                     </div>
                                                 </div>
